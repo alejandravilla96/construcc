5c31a4943eb080a7c5b7098e3daba33dbfd87099
Alejandra Sanchez Villa <88560398+alejandravilla96@users.noreply.github.com>
2021-08-27 22:37:32 -0500

    Add files via upload

---
+++ Pagina - Formulario/funciones.js
@@ -0,0 +1,15 @@
+function resgistro_user() {
+       
+
+
+    let nombre = document.getElementById ("nombre_user").Value
+    let apellido = document.getElementById("apellido:user").Value
+    let identificacion = document.getElementById("identificacion_user").Value
+    let numeropersonas= document.getElementById("numeropersonas_user").Value
+    let numerohabitacion = document.getElementById("numerohabitacion").Value
+    console.long(nombre + " " + apellido + " " + identificacion + " " + numeropersonas + " " + numerohabitacion)
+
+}
+
+var myModal = document.getElementById('myModal')
+
